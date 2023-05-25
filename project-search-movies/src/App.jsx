4b89725f5d274ee64389977fbe6495cba5a80e94
Erick Cs <60988586+ErickCSS@users.noreply.github.com>
2023-05-24 22:28:56 -0400

    [ADD] API Form and Styles

--- project-search-movies/src/App.jsx
+++ project-search-movies/src/App.jsx
@@ -1,11 +1,32 @@
+import './App.css'
+import resultsMovies from './mockup/resultsApi.json'
+import noResults from './mockup/noResults.json'
 
-
+// const URL = "https://www.omdbapi.com/?apikey=e843b93a&s=lapara"
+// const API_KEY="e843b93a"
 function App() {
-
+  const { Search: movies} = resultsMovies
+  const hasMovies = movies.length > 0
 
   return (
     <>
-      <h1>Prueba Técnica</h1>
+      <header>
+        <h1>Buscador de Películas</h1>
+        <form action="">
+          <input type="text"  placeholder="Marvel, Fast and Furious..."/>
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+      <main>
+        {
+          hasMovies && movies.map(movie => (
+            <article key={movie.imdbID}>
+              <img src={movie.Poster} alt={movie.Title}/>
+              <h3>{movie.Title} - {movie.Year}</h3>
+            </article>
+          ))
+        }
+      </main>
     </>
   )
 }
